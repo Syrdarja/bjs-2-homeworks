17b268d16c57ea7e0e63cd106a275dc04e4d5f0c
MariMax <maxim.prohachev@yandex.ru>
2024-07-17 18:29:53 +0300

    задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+
+	if (d > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+	} else if (d === 0) {
+		arr[0] = -b / (2 * a);
+	} else if (d < 0) {
+		arr = [];
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	let persentMonths = percent / 100 / 12; //процентная ставка
+	let loanBody = amount - contribution; //тело кредита
+
+	let monthlyPayment = loanBody * (persentMonths + (persentMonths / (Math.pow(1 + persentMonths, countMonths) - 1))); //ежемесячный платеж
   
+	let totalAmount = (monthlyPayment * countMonths).toFixed(2); //общая сумма кредита
+
+	return totalAmount = parseFloat(totalAmount);
 }
