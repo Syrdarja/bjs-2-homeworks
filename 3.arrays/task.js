24b79ec6eef3e3cbfaaa0a4153dde5b8fbcf6ea8
MariMax <maxim.prohachev@yandex.ru>
2024-07-25 19:49:34 +0300

    add 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+	if (
+		arr1.length === arr2.length &&
+		arr1.every((element, index) => element === arr2[index])
+	) {
+		return true;
+	}
+	return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let arr = users.filter(user => user.gender === gender).map(user => user.age)
+	let result = arr.reduce((sum,age) => sum += age,0);
+	if (result > 0) {
+		return result/arr.length;
+} else {
+	return 0;
+}
 }
