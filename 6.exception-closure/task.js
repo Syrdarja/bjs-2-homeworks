310b181c667fac687ffa33082e3adfa1e9f3023f
MariMax <maxim.prohachev@yandex.ru>
2024-08-02 18:45:07 +0300

    add 6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount (count){
+    let result = Number.parseFloat(count);
+   if (Number.isNaN(parseFloat(count))) {
+    throw new Error(`Невалидное значение`);
+}
+      return result;
+}
+
+
+    function validateCount (number) {
+        try {
+            parseCount(number);
+    } catch(error) {
+     return error
+    }
+}
