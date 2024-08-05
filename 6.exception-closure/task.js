85f420576c017afc7f7759dd1a4dff6dc2cb6fce
MariMax <maxim.prohachev@yandex.ru>
2024-08-05 20:12:46 +0300

    add 6.с ошибкой

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,16 +1,54 @@
-﻿function parseCount (count){
-    let result = Number.parseFloat(count);
-   if (Number.isNaN(parseFloat(count))) {
-    throw new Error(`Невалидное значение`);
-}
-      return result;
+﻿function parseCount(count) {
+	const result = Number.parseFloat(count);
+	if (Number.isNaN(parseFloat(count))) {
+		throw new Error(`Невалидное значение`);
+	}
+	return result;
 }
 
 
-    function validateCount (number) {
-        try {
-            parseCount(number);
-    } catch(error) {
-     return error
-    }
+function validateCount(number) {
+	try {
+		parseCount(number);
+	} catch (error) {
+		return error
+	}
 }
+
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if ((a + b < c) || (a + c < b) || (b + c < a)) {
+			throw new Error(`Треугольник с такими сторонами не существует`);
+		}
+	}
+	get perimeter() {
+		return (this.a + this.b + this.c);
+	}
+
+	get area() {
+		const p = this.perimeter / 2;
+		const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return +area.toFixed(3);
+	}
+}
+
+function getTriangle(a, b, c) {
+
+	try {
+		return new Triangle(a, b, c)
+	} catch (error) {
+		return {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		}
+	}
+}
+
