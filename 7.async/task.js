4c3aa8d682212374a6ed3961c063dc76dad9d69a
MariMax <maxim.prohachev@yandex.ru>
2024-08-10 14:55:12 +0300

    add 7.async

---
+++ 7.async/task.js
@@ -0,0 +1,59 @@
+class AlarmClock {
+	constructor(alarmCollection, intervalId) {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		} else {
+			if (this.alarmCollection.find(clock => clock.time === time)) {
+				console.warn('Уже присутствует звонок на это же время');
+			}
+			this.alarmCollection.push({
+				callback,
+				time,
+				canCall: true
+			});
+		}
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(
+			clock => clock.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		let formatTime = new Date();
+		return formatTime.toTimeString().slice(0, 5);
+	}
+
+	start() {
+		if (this.intervalId) {
+			return;
+		}
+		this.intervalId = setInterval(() => {
+			this.alarmCollection.forEach(clock => {
+				if (clock.time === this.getCurrentFormattedTime() && clock.canCall) {
+					clock.canCall = false;
+					clock.callback();
+				}
+			});
+		}, 1000);
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach(clock => clock.canCall = true);
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
